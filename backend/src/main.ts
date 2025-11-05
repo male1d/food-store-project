ce0e72fabd2bf756efda4a43524c23973fa97e11
male1d <104984547+male1d@users.noreply.github.com>
2025-11-05 17:20:55 +0500

    Fix backend port: change from 4000 to 3000

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,8 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  
-  await app.listen(4000);
-  console.log('🚀 Backend started on http://localhost:4000');
+  await app.listen(3000);
+  console.log('🚀 Backend started on http://localhost:3000');
 }
 bootstrap();
